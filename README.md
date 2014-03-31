efd3dda73368ecb987c939c2c1971efed07b7d6c
Marcos Cordeiro <marcoscbrito@gmail.com>
2014-03-31 18:23:12 -0300

    clean code

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ Projetos de python - coursera
 Rock-paper-scissors-lizard-Spock
 ---------------------------------
 
-http://www.codeskulptor.org/#user29_vvOwNgWoUK_1.py
+http://www.codeskulptor.org/#user29_vvOwNgWoUK_2.py
--- rpsls.py
+++ rpsls.py
@@ -15,29 +15,29 @@ import random
 
 # helper functions
 def name_to_number(name):
-    if name=="rock":
+    if name == "rock":
         return 0
-    elif name=="Spock":
+    elif name == "Spock":
         return 1
-    elif name=="paper":
+    elif name == "paper":
         return 2
-    elif name=="lizard":
+    elif name == "lizard":
         return 3
-    elif name=="scissors":
+    elif name == "scissors":
         return 4
     else:
         print "Please, enter a correct name!"
 
 def number_to_name(number):
-    if number==0:
+    if number == 0:
         return "rock"
-    elif number==1: 
+    elif number == 1: 
         return "Spock"
-    elif number==2:
+    elif number == 2:
         return "paper"
-    elif number==3:
+    elif number == 3:
         return "lizard"
-    elif number==4:
+    elif number == 4:
         return "scissors"
     else:
         print "Please, enter a correct number!"
